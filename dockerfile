ab2a6cd539c54409162d090f2c889509a32fb3a6
YEUNU <grinteacake@naver.com>
2023-10-09 17:37:57 +0900

    docker_update

---
+++ dockerfile
@@ -0,0 +1,19 @@
+# 베이스 이미지로 Python 3 기반 이미지 선택
+FROM python:3.11
+
+# 환경 변수 설정
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# 작업 디렉토리 설정
+WORKDIR /app
+
+# 필요한 패키지 설치
+RUN apt-get update && apt-get install -y git && rm -rf /var/lib/apt/lists/*
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# 소스 코드 복사(로컬에서 Docker 컨테이너 내부로)
+COPY portfolio .
+# gunicorn을 사용하여 Django 애플리케이션 실행 명령
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django_bootstrap5',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+# print(BASE_DIR)
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'main/static']
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'main/static'),
+)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+
