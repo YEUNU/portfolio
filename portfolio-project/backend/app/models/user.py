00c6c93f9db1fc95b73872e2fbb5f7ac9b795d49
성연우 <sung@seong-yeon-uui-MacBookAir.local>
2025-10-01 22:58:57 +0900

    user

--- portfolio-project/backend/app/models/user.py
+++ portfolio-project/backend/app/models/user.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+
 from app.db.base import Base
 
 class User(Base):
@@ -7,4 +9,8 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_admin = Column(Boolean(), default=False)
+
+    # --- [핵심] User와 Board 모델의 양방향 관계 설정 ---
+    # 이 부분이 없으면, Board 모델의 owner 관계가 짝을 찾지 못해 에러가 발생합니다.
+    posts = relationship("Board", back_populates="owner")
 
